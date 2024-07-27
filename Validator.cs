91ada526884bae7bce4b283748199dec5ef073fb
AmmarElsamman <elsamman321@gmail.com>
2024-07-27 14:17:20 +0300

    first commit

---
+++ Validator.cs
@@ -0,0 +1,39 @@
+﻿namespace DrinksInfo
+{
+    public class Validator
+    {
+        internal static bool IsIdValid(string stringInput)
+        {
+            if (String.IsNullOrEmpty(stringInput))
+            {
+                return false;
+            }
+
+            foreach (var c in stringInput)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        internal static bool IsStringValid(string stringInput)
+        {
+            if (String.IsNullOrEmpty(stringInput))
+            {
+                return false;
+            }
+
+            foreach (char c in stringInput)
+            {
+                if (!Char.IsLetter(c) && c != '/' && c != ' ')
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
